5d30e474deacc3a3773696a5eb66fa1a8517922d
Kartikey Tyagi <kartikeytyagi@Kartikeys-MacBook-Air.local>
2024-11-27 00:05:25 +0530

    feat: intial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import  Home  from "./pages/Home";
+import Movie from './pages/Movie'
+
+function App() {
+  
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={Home}></Route>
+        <Route path='/Movie' element={<Movie/>} ></Route>
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
